730ebdc659a9766df13824c7df5114762d9132d1
deviowa <deviowa@Caseys-MacBook-Air.local>
2014-06-06 15:48:32 -0500

    MOVING STUFF AND THINGS

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+$(window).on("scroll resize", function(){
+    var pos=$('#cwindow').offset();
+    $('.cpanel').each(function(){
+        if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top)
+        {
+            $('#cwindow').html($(this).html());
+            $('#cwindow').css("transform","translateX("+$(this).index() * 100+"px)");
+            return; //break the loop
+        }
+    });
+});
+
+$(document).ready(function(){
+  $(window).trigger('scroll'); // init the value
+});
+
+/*
 $(document).ready(function(){
     
     $('.splash').click(function(){
@@ -5,5 +22,5 @@ $(document).ready(function(){
         $(this).fadeTo("slow",0);
     });   
     
-    $('#menu').accordion();
 });
+*/
